fb2e189657a95040c2bf9b2ccbf1f9a30bc9fd9e
tymothykiogora <67871271+tymothykiogora@users.noreply.github.com>
2021-09-07 00:55:31 +0300

    Nest quote details using quote detail selector

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,28 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
-
 @Component({
-  selector: 'app-quote-detail',
-
-    
-        
-          
-    
-
-        
-    
-    @@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
-  
-  templateUrl: './quote-detail.component.html',
-  styleUrls: ['./quote-detail.component.css']
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
 })
-export class QuoteDetailComponent implements OnInit {
+export class QuoteComponent implements OnInit {
 
-  //input property binding
-  @Input()quote!: Quote;
+  quotes:Quote[] = [
+    new Quote(1,'A website without visitors is like a ship lost in the horizon','Dr. Christopher Dayagdag','Maureen'),
+    new Quote(2,'A user interface is like a joke. If you have to explain it, it is not that good','Unknown', 'Maureen'),
+    new Quote(3,'A designer knows he has achieved perfection not when there is nothing left to add, but when there is nothing left to take away','Antonie De-Saint Exupery','Maureen'),
+    new Quote(4,'Great web design without functionality is like a sports car with no engine','Paul Cookson','Maureen'),
+    new Quote(5,'It is not a bug, it is an undocumented feature!','Anonymous','Maureen'),
+  ]
+
   constructor() { }
-
   ngOnInit(): void {
+  }
+}
 
     
           
