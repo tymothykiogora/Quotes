00b340c572aa9544982a88ecb7e2d8b7a5b49a9c
tymothykiogora <67871271+tymothykiogora@users.noreply.github.com>
2021-09-07 00:49:24 +0300

    Define constructor and a boolean variable for showing vote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
+    showVotes:boolean;
+    constructor(public id: number, public author: string, public statement: string, public createdBy:string) {
+        this.showVotes=false;
+    }
 }
