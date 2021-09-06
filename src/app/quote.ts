6bb9b2723bd0860989f60e5999151f0da15842e5
tymothykiogora <67871271+tymothykiogora@users.noreply.github.com>
2021-09-07 01:18:39 +0300

    add likes and dislikes property

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,7 @@
 export class Quote {
     showVotes:boolean;
+    likes!: number;
+    dislikes!: number;
     constructor(public id: number, public statement: string, public author: string, public createdBy:string) {
         this.showVotes=false;
     }
