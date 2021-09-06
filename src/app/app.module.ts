5dc330b3a8ba6fc0bf0e08e38aacd6e88166b3bb
tymothykiogora <67871271+tymothykiogora@users.noreply.github.com>
2021-09-07 02:47:49 +0300

    create form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailComponent,
+    QuoteFormComponent,
   ],
   imports: [
     BrowserModule,
