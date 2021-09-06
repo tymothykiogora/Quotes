f39fc148f7cdd5e9865a1eb31ffb1867814cc47f
tymothykiogora <67871271+tymothykiogora@users.noreply.github.com>
2021-09-07 01:15:05 +0300

    Define function for toggling vote details and incorporate in child selector component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -6,7 +6,6 @@ import { Quote } from '../quote';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
   quotes:Quote[] = [
     new Quote(1,'A website without visitors is like a ship lost in the horizon','Dr. Christopher Dayagdag','Maureen'),
     new Quote(2,'A user interface is like a joke. If you have to explain it, it is not that good','Unknown', 'Maureen'),
@@ -14,20 +13,14 @@ export class QuoteComponent implements OnInit {
     new Quote(4,'Great web design without functionality is like a sports car with no engine','Paul Cookson','Maureen'),
     new Quote(5,'It is not a bug, it is an undocumented feature!','Anonymous','Maureen'),
   ]
+  goals: any;
+
+  toggleDetails(index: number) {
+    this.goals[index].showVotes = !this.goals[index].showVotes;
+  }
 
   constructor() { }
+
   ngOnInit(): void {
-  }
-}
-
-    
-          
-            
-    
-
-          
-    
-    
-  
   }
 }
